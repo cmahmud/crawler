e1d5fc4c5e3d590a6cc53f494a16360cc79e345a
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-23 22:20:00 -0400

    feat: expose expiring Redis frontier

--- src/syndcrawler/storage/__init__.py
+++ src/syndcrawler/storage/__init__.py
@@ -1,6 +1,6 @@
 from syndcrawler.storage.postgres_recrawl_claims import PostgresRecrawlClaims, RecrawlClaim
 from syndcrawler.storage.postgres_store import PostgresCrawlStore
-from syndcrawler.storage.redis_frontier import RedisFrontier
+from syndcrawler.storage.redis_store import RedisFrontier
 from syndcrawler.storage.sqlite_crawl_store import (
     CrawlManifest,
     ResourceState,
