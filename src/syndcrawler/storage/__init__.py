9beb0ceb6d210b42658d22d4c3a85301fad421ff
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-23 22:01:26 -0400

    feat: export distributed recrawl claims

--- src/syndcrawler/storage/__init__.py
+++ src/syndcrawler/storage/__init__.py
@@ -1,4 +1,5 @@
 from syndcrawler.storage.postgres_crawl_store import PostgresCrawlStore
+from syndcrawler.storage.postgres_recrawl_claims import PostgresRecrawlClaims, RecrawlClaim
 from syndcrawler.storage.redis_frontier import RedisFrontier
 from syndcrawler.storage.sqlite_crawl_store import (
     CrawlManifest,
@@ -10,6 +11,8 @@ from syndcrawler.storage.sqlite_frontier import SQLiteFrontier
 __all__ = [
     "CrawlManifest",
     "PostgresCrawlStore",
+    "PostgresRecrawlClaims",
+    "RecrawlClaim",
     "RedisFrontier",
     "ResourceState",
     "SQLiteCrawlStore",
