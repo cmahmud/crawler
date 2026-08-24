58d927a3cca07f56e328ac42ed2be4804786bdbd
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-23 20:13:47 -0400

    feat: add discovery package

---
+++ src/syndcrawler/discovery/__init__.py
@@ -0,0 +1,15 @@
+from syndcrawler.discovery.documents import (
+    DiscoveryDocument,
+    DiscoveryParseError,
+    parse_feed,
+    parse_robots_sitemaps,
+    parse_sitemap,
+)
+
+__all__ = [
+    "DiscoveryDocument",
+    "DiscoveryParseError",
+    "parse_feed",
+    "parse_robots_sitemaps",
+    "parse_sitemap",
+]
