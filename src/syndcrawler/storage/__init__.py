acc54f758d50bf9e4f57dcb9cd8689579606f673
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-23 20:07:42 -0400

    feat: export crawl state store

--- src/syndcrawler/storage/__init__.py
+++ src/syndcrawler/storage/__init__.py
@@ -1,3 +1,4 @@
+from syndcrawler.storage.sqlite_crawl_store import CrawlManifest, SQLiteCrawlStore
 from syndcrawler.storage.sqlite_frontier import SQLiteFrontier
 
-__all__ = ["SQLiteFrontier"]
+__all__ = ["CrawlManifest", "SQLiteCrawlStore", "SQLiteFrontier"]
