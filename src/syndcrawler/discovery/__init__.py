b0885b8c01a51426ed8c3c108e417d32d7a70db0
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-23 20:20:20 -0400

    feat: export sitemap discovery client

--- src/syndcrawler/discovery/__init__.py
+++ src/syndcrawler/discovery/__init__.py
@@ -1,3 +1,9 @@
+from syndcrawler.discovery.client import (
+    DiscoveredUrl,
+    DiscoveryFailure,
+    SitemapDiscoveryClient,
+    SitemapDiscoveryResult,
+)
 from syndcrawler.discovery.documents import (
     DiscoveryDocument,
     DiscoveryParseError,
@@ -7,8 +13,12 @@ from syndcrawler.discovery.documents import (
 )
 
 __all__ = [
+    "DiscoveredUrl",
     "DiscoveryDocument",
+    "DiscoveryFailure",
     "DiscoveryParseError",
+    "SitemapDiscoveryClient",
+    "SitemapDiscoveryResult",
     "parse_feed",
     "parse_robots_sitemaps",
     "parse_sitemap",
