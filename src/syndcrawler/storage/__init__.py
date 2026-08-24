7b86ca53e9005ff6173ad4937ce8c60e79e2252f
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-23 20:40:54 -0400

    chore: export durable resource state

--- src/syndcrawler/storage/__init__.py
+++ src/syndcrawler/storage/__init__.py
@@ -1,4 +1,13 @@
-from syndcrawler.storage.sqlite_crawl_store import CrawlManifest, SQLiteCrawlStore
+from syndcrawler.storage.sqlite_crawl_store import (
+    CrawlManifest,
+    ResourceState,
+    SQLiteCrawlStore,
+)
 from syndcrawler.storage.sqlite_frontier import SQLiteFrontier
 
-__all__ = ["CrawlManifest", "SQLiteCrawlStore", "SQLiteFrontier"]
+__all__ = [
+    "CrawlManifest",
+    "ResourceState",
+    "SQLiteCrawlStore",
+    "SQLiteFrontier",
+]
